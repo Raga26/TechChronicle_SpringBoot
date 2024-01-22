31aecef43b3ef1024fe18470f4754c46022a5506
Ragamaliga <mragamaliga@gmail.com>
2024-01-22 13:38:49 +0530

    commit with swagger documentation

--- src/main/java/com/example/springBootblogrestapi/SpringBootBlogRestApiApplication.java
+++ src/main/java/com/example/springBootblogrestapi/SpringBootBlogRestApiApplication.java
@@ -1,11 +1,18 @@
 package com.example.springBootblogrestapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "TechChronicle - SpringBoot RESTful API"
+		)
+)
 public class SpringBootBlogRestApiApplication {
 	@Bean
 	public ModelMapper modelMapper(){
